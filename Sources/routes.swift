fac223703c1d12d130360f493b9bb1de41d08693
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-11-27 11:49:50 +0000

    undo

--- Sources/routes.swift
+++ Sources/routes.swift
@@ -3,7 +3,7 @@ import Vapor
 
 func ConfigureRoutes(app: Application) {
     
-    app.post("/") { req in
+    app.post("") { req in
         print("request at /")
         return Response(status: .ok, body: "Hello World")
     }
