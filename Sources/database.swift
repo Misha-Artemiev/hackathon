2e7acb358a07bf1b25e144aa8a02a025a92f993f
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-11-27 11:58:35 +0000

    added user table

---
+++ Sources/database.swift
@@ -0,0 +1,61 @@
+import Fluent
+import FluentPostgresDriver
+import Vapor
+
+func configureConnection(_ app: Application) {
+    app.databases.use(
+        .postgres(
+            configuration: .init(
+                hostname: "localhost",
+                port: 5440,
+                username: "hackathonconnect",
+                password: "vRexyDivQ5040MS",
+                database: "hackathon",
+                tls: .disable
+            )
+        ),
+        as: .psql
+    )
+    
+    app.migrations.add([CreateUsersTable()])
+}
+
+struct CreateUsersTable: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users")
+            .id()
+            .field("username", .string)
+            .field("password", .string)
+            .field("created", .datetime)
+            .create()
+    }
+    
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("users").delete()
+    }
+}
+
+final class User: Model, Content, @unchecked Sendable {
+    
+    static let schema: String = "users"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "username")
+    var username: String
+    
+    @Field(key: "password")
+    var password: String
+    
+    @Timestamp(key: "created", on: .create)
+    var created: Date?
+    
+    init() {}
+    
+    init(id: UUID? = nil, username: String, password: String) {
+        self.id = id
+        self.username = username
+        self.password = password
+    }
+}
